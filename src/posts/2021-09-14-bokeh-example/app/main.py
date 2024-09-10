9463af155464f542861190a2607ccc8356ee9cd8
Troels Laegsgaard <laegsgaardtroels@gmail.com>
2024-09-10 12:38:44 +0200

    Minor changes

--- src/posts/2021-09-14-bokeh-example/app/main.py
+++ src/posts/2021-09-14-bokeh-example/app/main.py
@@ -1,9 +1,6 @@
-from bokeh import events
+from bokeh.io import curdoc
 from bokeh import layouts
 from bokeh import models as bm
-
-import math
-import requests
 
 from app import model
 from app import views
@@ -14,13 +11,23 @@ class Main:
         self.model = model.Model()
         self.map = views.WorldMap(self.model)
         self.setup_controls()
-        self.layout = layouts.row(children=[])
+        self.layout = layouts.column(children=[])
 
     def refresh(self):
         self.map.refresh()
         self.layout.children = [
-            self.controls,
-            self.map.layout,
+            bm.Div(
+                text=(
+                    "<h1>Real Estate Valuation</h1>"
+                    "<p1>A small application that can estimate a fair price on real estate and display the prices on a map.</p1>"
+                ),
+            ),
+            layouts.row(
+                children=[
+                    self.controls,
+                    self.map.layout,
+                ]
+            ),
         ]
         self.div_spinner.text = ""
 
@@ -31,31 +38,27 @@ class Main:
             options=self.model.zipcode_options,
         )
         zipcode.on_change(
-            'value',
+            "value",
             self.zipcode_handler,
         )
         self.div_spinner = bm.widgets.Div(text="", width=10, height=10)
         self.controls = layouts.row(
-            children=[
-                zipcode,
-                self.div_spinner
-            ],
-            width=400
-        )
+            children=[zipcode, self.div_spinner], width=400)
 
     def zipcode_handler(self, attr, old, new):
         spinner_text = '<div class="loader"></div>'
         self.div_spinner.text = spinner_text
+
         def update():
             self.model.selected_zipcode = new
             x_min, y_min, x_max, y_max = self.model.xybbox
             self.map.plot.x_range.update(start=x_min, end=x_max)
             self.map.plot.y_range.update(start=y_min, end=y_max)
             self.div_spinner.text = ""
+
         curdoc().add_next_tick_callback(update)
 
 
-from bokeh.io import curdoc
 main = Main()
 main.refresh()
 curdoc().add_root(main.layout)
